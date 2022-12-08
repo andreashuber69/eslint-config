d1daccab6c0f414df6b152df4e8b14edc34ea625
Andreas Huber <andreashuber69@users.noreply.github.com>
2022-12-08 20:29:32 +0100

    chore: complete list of import rules

--- index.js
+++ index.js
@@ -91,9 +91,11 @@ module.exports = {
         "import/dynamic-import-chunkname": "error",
         "import/export": "off", // Already covered by typescript.
         "import/exports-last": "error",
+        "import/extensions": "off", // Already covered by typescript.
         "import/first": "error",
         // There are advantages and disadvantages to turning this on or off. "off" seems the better choice.
         "import/group-exports": "off",
+        "import/imports-first": "off", // Deprecated in favor of import/first
         "import/max-dependencies": [
             "error",
             {
@@ -124,13 +126,16 @@ module.exports = {
         "import/no-duplicates": "error",
         "import/no-dynamic-require": "error",
         "import/no-extraneous-dependencies": "error",
+        "import/no-import-module-exports": "error",
         "import/no-internal-modules": "off", // Seems too restrictive.
         "import/no-mutable-exports": "error",
-        "import/no-named-as-default": "error",
         "import/no-named-as-default-member": "error",
+        "import/no-named-as-default": "error",
         "import/no-named-default": "error",
         "import/no-named-export": "off", // Does not make sense.
         "import/no-namespace": "error",
+        "import/no-nodejs-modules": "off",
+        "import/no-relative-packages": "error",
         "import/no-relative-parent-imports": "off", // Seems too restrictive.
         "import/no-restricted-paths": "off", // Seems too restrictive.
         "import/no-self-import": "error",
