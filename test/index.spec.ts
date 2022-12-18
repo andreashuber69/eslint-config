10b7a58a1ef37a6233cb8418d362e4bab047ea0e
Andreas Huber <andreashuber69@users.noreply.github.com>
2022-12-18 16:03:22 +0100

    refactor: clarify code & comments

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,7 +1,7 @@
 import { isDeepStrictEqual } from "util";
 import { expect } from "chai";
 
-import { rules as ourRules } from "../index";
+import { rules as ourChanges } from "../index";
 
 import { allEslintRules } from "./allEslintRules";
 import { allImportRules } from "./allImportRules";
@@ -14,13 +14,13 @@ const allEsTsRules = { ...allEslintRules, ...allTseslintRules };
 
 describe("index.js", () => {
     describe("should change the default of ES and TS rules", () => {
-        // Since our rules extend from both the "eslint:all" and "plugin:@typescript-eslint/all" configs, we
-        // need to test that our rules are listed in allEsTsRules and apply a config that is different from the
-        // default.
-        const ourEsTsRules =
-            Object.entries(ourRules).filter(([id]) => id.includes("@typescript-eslint/") || !id.includes("/"));
+        // Since ../index.js extends from both the "eslint:all" and "plugin:@typescript-eslint/all" configs, we need
+        // to test that the rule ids in ourChanges are listed in allEsTsRules and that we apply severity/options that
+        // are different.
+        const ourEsTsChanges =
+            Object.entries(ourChanges).filter(([id]) => id.includes("@typescript-eslint/") || !id.includes("/"));
 
-        for (const [id, ourEntry] of ourEsTsRules) {
+        for (const [id, ourEntry] of ourEsTsChanges) {
             it(id, () => {
                 const entry = allEsTsRules[id];
                 expect(Boolean(entry)).to.equal(true, `${id} is not in the list of extended from rules.`);
@@ -34,16 +34,29 @@ const allOtherRules = { ...allImportRules, ...allJsdocRules, ...allNonullRules, 
 
 describe("index.js", () => {
     describe("should list all other rules", () => {
-        // Our rules don't extend from any config related to allOtherRules. For these we simply test that we have all
-        // these rules in our list. Doing that ensures that we will not miss a newly added rule.
-        const ourOtherRules = Object.fromEntries(
-            Object.entries(ourRules).filter(([id]) => !id.includes("@typescript-eslint/") && id.includes("/")),
+        // ../index.js doesn't extend from any config related to allOtherRules. For these we test that *all* rule ids
+        // are listed in ourChanges. While we would not need to list rules that we turn off, doing so ensures that we
+        // will be alerted when rules are added in subsequent versions. We can then consciously either turn these off or
+        // set the severity/options we need.
+        const ourOtherChanges = Object.fromEntries(
+            Object.entries(ourChanges).filter(([id]) => !id.includes("@typescript-eslint/") && id.includes("/")),
         );
 
         for (const id of Object.keys(allOtherRules)) {
             it(id, () => {
-                expect(Boolean(ourOtherRules[id])).to.equal(true, `${id} is not in the list of other rules.`);
+                expect(Boolean(ourOtherChanges[id])).to.equal(true, `${id} is not in the list of other rules.`);
             });
         }
     });
 });
+
+const allRules = { ...allEsTsRules, ...allOtherRules };
+
+describe(`${Object.keys(allRules).length} rules`, () => {
+    it("Default severity should be 'off' or 'error', without options", () => {
+        expect(Object.values(allRules).filter((v) => v !== "off" && v !== "error").length).to.equal(0);
+    });
+});
+
+const activeRules = Object.entries({ ...allRules, ...ourChanges }).filter(([, entry]) => entry !== "off");
+console.log(`@andreashuber/eslint-config active rules: ${activeRules.length}`);
