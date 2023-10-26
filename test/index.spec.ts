46e1fd97862e94c46a04b6a3aa9159160fcfa93f
Andreas Huber <andreashuber69@users.noreply.github.com>
2023-10-26 18:31:26 +0200

    Revert "chore: update packages to current lts (20)"
    This reverts commit 04777ecab63ce175579fbf6aecd06c402ea9e9f1.

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -15,63 +15,49 @@ import { allTypescriptEslintRules, recommendedTypescriptEslintRules } from "./ty
 
 const allRulesInConfig = { ...allEslintRules, ...allTypescriptEslintRules, ...allUnicornRules };
 const allOtherRules = { ...allImportRules, ...allJsdocRules, ...allPreferArrowRules, ...allPromiseRules };
-const allRules = { ...allRulesInConfig, ...allOtherRules };
 
 const shouldRuleBeInConfig = ([id]: [string, ...unknown[]]) =>
     id.includes("@typescript-eslint/") || id.includes("unicorn/") || !id.includes("/");
 
-const hasAllKeys = async (original: Record<string, unknown>, tester: Record<string, unknown>, message: string) => {
+const hasAllKeys = (original: Record<string, unknown>, tester: Record<string, unknown>, message: string) => {
     for (const id of Object.keys(original)) {
-        // eslint-disable-next-line no-await-in-loop
-        await it(id, () => assert(Boolean(tester[id]), `${id} ${message}`));
+        it(id, () => assert(Boolean(tester[id]), `${id} ${message}`));
     }
 };
 
-describe("all", async () => {
-    await describe("index.js", async () => {
-        await describe("should change the defaults of rules listed in 'all' configs", async () => {
-            const ourRuleInConfigChanges = Object.entries(ourChanges).filter((e) => shouldRuleBeInConfig(e));
+describe("index.js", () => {
+    describe("should change the defaults of rules listed in 'all' configs", () => {
+        const ourRuleInConfigChanges = Object.entries(ourChanges).filter((e) => shouldRuleBeInConfig(e));
 
-            // Since ../index.js extends from the "eslint:all", "plugin:@typescript-eslint/all" and "plugin:unicorn/all"
-            // configs, we need to test that the rule ids in ourChanges are listed in allRulesInConfig and that we apply
-            // severity/options that are different.
-            for (const [id, ourEntry] of ourRuleInConfigChanges) {
-                // eslint-disable-next-line no-await-in-loop
-                await it(id, () => {
-                    const entry = allRulesInConfig[id];
-                    assert(Boolean(entry), `${id} is not in the list of extended from rules.`);
-                    assert(!isDeepStrictEqual(ourEntry, entry), `${id} does not change the default.`);
-                });
-            }
-        });
-
-        const ourOtherChanges = Object.fromEntries(Object.entries(ourChanges).filter((e) => !shouldRuleBeInConfig(e)));
-
-        // ../index.js doesn't extend from any config related to allOtherRules. For these we test that *all* rule ids
-        // are listed in ourChanges and vice versa. While we would not need to list rules that we turn off, doing so
-        // ensures that we will be alerted when rules are added in subsequent versions. We can then consciously either
-        // turn these off or set the severity/options we need.
-        await describe(
-            "should list all other rules",
-            async () => await hasAllKeys(allOtherRules, ourOtherChanges, "is not in index.js."),
-        );
-
-        await describe(
-            "should not list unknown rules",
-            async () => await hasAllKeys(ourOtherChanges, allOtherRules, "is an unknown rule."),
-        );
+        // Since ../index.js extends from the "eslint:all", "plugin:@typescript-eslint/all" and "plugin:unicorn/all"
+        // configs, we need to test that the rule ids in ourChanges are listed in allRulesInConfig and that we apply
+        // severity/options that are different.
+        for (const [id, ourEntry] of ourRuleInConfigChanges) {
+            it(id, () => {
+                const entry = allRulesInConfig[id];
+                assert(Boolean(entry), `${id} is not in the list of extended from rules.`);
+                assert(!isDeepStrictEqual(ourEntry, entry), `${id} does not change the default.`);
+            });
+        }
     });
 
-    await describe(`${Object.keys(allRules).length} rules`, async () => {
-        await it("Default severity should be 'off' or 'error', without options", () => {
-            assert(Object.values(allRules).filter((v) => v !== "off" && v !== "error").length === 0);
-        });
-    });
-// This is a CommonJS module, where top-level await is not available. Compiling tests differently is possible but not
-// worth the effort.
-// eslint-disable-next-line unicorn/prefer-top-level-await
-}).catch((error) => console.error(error));
+    const ourOtherChanges = Object.fromEntries(Object.entries(ourChanges).filter((e) => !shouldRuleBeInConfig(e)));
 
+    // ../index.js doesn't extend from any config related to allOtherRules. For these we test that *all* rule ids
+    // are listed in ourChanges and vice versa. While we would not need to list rules that we turn off, doing so ensures
+    // that we will be alerted when rules are added in subsequent versions. We can then consciously either turn these
+    // off or set the severity/options we need.
+    describe("should list all other rules", () => hasAllKeys(allOtherRules, ourOtherChanges, "is not in index.js."));
+    describe("should not list unknown rules", () => hasAllKeys(ourOtherChanges, allOtherRules, "is an unknown rule."));
+});
+
+const allRules = { ...allRulesInConfig, ...allOtherRules };
+
+describe(`${Object.keys(allRules).length} rules`, () => {
+    it("Default severity should be 'off' or 'error', without options", () => {
+        assert(Object.values(allRules).filter((v) => v !== "off" && v !== "error").length === 0);
+    });
+});
 
 const getActiveCount = (rules: Record<string, unknown>) => Object.values(rules).filter((v) => v !== "off").length;
 
