5a2440dd2409958c7dc42eb2a60c39a0b33fcea7
Andreas Huber <andreashuber69@users.noreply.github.com>
2023-10-28 13:03:21 +0200

    feat: allow camel and pascal case for imports

--- index.js
+++ index.js
@@ -413,6 +413,15 @@ module.exports = {
                 trailingUnderscore: "forbid",
             },
             {
+                selector: ["import"],
+                format: [
+                    "StrictPascalCase",
+                    "strictCamelCase",
+                ],
+                leadingUnderscore: "forbid",
+                trailingUnderscore: "forbid",
+            },
+            {
                 selector: [
                     "parameter",
                     "variable",
