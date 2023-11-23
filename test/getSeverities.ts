a01eba2e1930fbdddad6e1aadd8d9d8b29b89711
Andreas Huber <andreashuber69@users.noreply.github.com>
2023-11-23 23:18:46 +0100

    fix: rules turned off with [0]

--- test/getSeverities.ts
+++ test/getSeverities.ts
@@ -1,21 +1,17 @@
 // https://github.com/andreashuber69/eslint-config/blob/master/README.md#----andreashuber69eslint-config
 import type { TSESLint } from "@typescript-eslint/utils";
 
-type RuleModule = TSESLint.RuleModule<string, unknown[]>;
-
-type RuleEntry = RuleModule | TSESLint.Linter.RuleEntry | undefined;
-
-const getRuleLevel = (entry: RuleEntry) => {
+const getRuleLevel = (entry: unknown) => {
     switch (typeof entry) {
         case "number":
         case "string":
             return entry;
         default:
-            return (Array.isArray(entry) && entry[0]) || "error";
+            return Array.isArray(entry) && entry.length ? entry[0] : "error";
     }
 };
 
-const getSeverityString = (entry: RuleEntry): TSESLint.Linter.SeverityString => {
+const getSeverityString = (entry: unknown): TSESLint.Linter.SeverityString => {
     const ruleLevel = getRuleLevel(entry);
 
     switch (ruleLevel) {
@@ -30,12 +26,12 @@ const getSeverityString = (entry: RuleEntry): TSESLint.Linter.SeverityString => 
     }
 };
 
-export const getSeverities = (rules: Record<string, RuleEntry> | undefined, prefix = "") => {
+export const getSeverities = (rules: unknown, prefix = "") => {
     const result: Record<string, unknown> = {};
 
-    if (rules) {
+    if (rules && typeof rules === "object") {
         for (const id of Object.keys(rules)) {
-            result[`${prefix}${prefix && "/"}${id}`] = getSeverityString(rules[id]);
+            result[`${prefix}${prefix && "/"}${id}`] = getSeverityString((rules as Record<string, unknown>)[id]);
         }
     }
 
