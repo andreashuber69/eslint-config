e87e357a748e7da6e46c106f1920093b2ba97ca9
Andreas Huber <andreashuber69@users.noreply.github.com>
2023-12-20 20:15:41 +0100

    test: reorder, sort and check variants

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -46,7 +46,7 @@ const getAllConfigsRules = async () => await getRules({
 
 const allOtherRules = { ...allImportRules, ...allJsdocRules, ...allPreferArrowRules, ...allPromiseRules };
 
-const isInAllConfigs = ([id]: [string, ...unknown[]]) =>
+const isInAllConfigs = ([id]: [string, unknown]) =>
     id.includes("@typescript-eslint/") || id.includes("@stylistic/") || id.includes("unicorn/") || !id.includes("/");
 
 const hasAllKeys = async (original: Record<string, unknown>, tester: Record<string, unknown>, message: string) => {
@@ -58,6 +58,15 @@ const hasAllKeys = async (original: Record<string, unknown>, tester: Record<stri
 
 // eslint-disable-next-line promise/always-return
 getAllConfigsRules().then(async (allConfigsRules) => {
+    await describe("All rules", async () => {
+        await describe("Default severity should be 'off' or 'error', without options", async () => {
+            for (const [id, severity] of sort({ ...allConfigsRules, ...allOtherRules })) {
+                // eslint-disable-next-line no-await-in-loop
+                await it(`"${id}": "${severity}"`, () => assert(severity === "off" || severity === "error"));
+            }
+        });
+    });
+
     await describe("index.js", async () => {
         // According to https://eslint.style/guide/config-presets#enable-all-avaible-rules,
         // plugin:@stylistic/all-extends deliberately does not include non-fixable rules, we therefore need to
@@ -80,8 +89,10 @@ getAllConfigsRules().then(async (allConfigsRules) => {
             }
         });
 
+        const ourAllConfigsChanges = Object.fromEntries(Object.entries(ourChanges).filter((e) => isInAllConfigs(e)));
+
         await describe("should change the defaults of rules listed in 'all' configs", async () => {
-            for (const [id, ourEntry] of sort(ourChanges).filter((e) => isInAllConfigs(e))) {
+            for (const [id, ourEntry] of sort(ourAllConfigsChanges)) {
                 if (!nonFixableStylisticRuleIds.includes(id)) {
                     // eslint-disable-next-line no-await-in-loop
                     await it(id, () => {
@@ -97,7 +108,7 @@ getAllConfigsRules().then(async (allConfigsRules) => {
             }
         });
 
-        const ourOtherChanges = Object.fromEntries(sort(ourChanges).filter((e) => !isInAllConfigs(e)));
+        const ourOtherChanges = Object.fromEntries(Object.entries(ourChanges).filter((e) => !isInAllConfigs(e)));
 
         // ../index.js doesn't extend from any config related to allOtherRules. For these we test that *all* rule ids
         // are listed in ourChanges and vice versa. While we would not need to list rules that we turn off, doing so
@@ -114,14 +125,42 @@ getAllConfigsRules().then(async (allConfigsRules) => {
         );
     });
 
-    await describe("All rules", async () => {
-        await describe("Default severity should be 'off' or 'error', without options", async () => {
-            for (const [id, severity] of sort({ ...allConfigsRules, ...allOtherRules })) {
+    await describe("Modified rules", async () => {
+        await describe("should activate at most one variant of a given rule", async () => {
+            const exceptions = new Set([
+                "no-lonely-if",
+                // Adds to no-lonely-if, see
+                // https://github.com/sindresorhus/eslint-plugin-unicorn/blob/main/docs/rules/no-lonely-if.md
+                "unicorn/no-lonely-if",
+                // Same name, but address completely different issues
+                "@typescript-eslint/no-namespace",
+                "import/no-namespace",
+                // Besides some overlap flag different constructs that can be converted to includes()
+                "@typescript-eslint/prefer-includes",
+                "unicorn/prefer-includes",
+                "prefer-spread",
+                // Adds to prefer-spread, see
+                // https://github.com/sindresorhus/eslint-plugin-unicorn/blob/main/docs/rules/prefer-spread.md
+                "unicorn/prefer-spread",
+            ]);
+
+
+            const rulesToTest = Object.fromEntries(
+                Object.entries(await getRules()).filter(([i, s]) => s !== "off" && !exceptions.has(i)),
+            );
+
+            const lookup = new Map<string, string>();
+
+            for (const [id] of sort(rulesToTest)) {
+                const stripped = strip(id);
+                const existing = lookup.get(stripped);
                 // eslint-disable-next-line no-await-in-loop
-                await it(`"${id}": "${severity}"`, () => assert(severity === "off" || severity === "error"));
+                await it(id, () => assert(existing === undefined, `Variant ${existing} is already active.`));
+                lookup.set(stripped, id);
             }
         });
     });
+
 // eslint-disable-next-line unicorn/prefer-top-level-await
 }).catch((error) => console.error(error));
 
