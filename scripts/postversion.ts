e541a5923fca2bf140fff3b772230eb54c78b8f7
Andreas Huber <andreashuber69@users.noreply.github.com>
2024-12-26 14:25:51 +0100

    docs: clarify

--- scripts/postversion.ts
+++ scripts/postversion.ts
@@ -5,4 +5,6 @@ import { exec } from "./exec.js";
 
 const { version } = packageJson;
 
+// NOTE: For this script to work, either the option --no-git-tag-version needs to be added to the npm version command or
+// the npm configuration needs to be modified as follows: npm config set git-tag-version=false
 await exec(`git commit -a -m "chore: start release v${version}"`);
