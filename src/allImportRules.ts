3ea746bd8136877f66ae09f6740b49936c877822
Andreas Huber <andreashuber69@users.noreply.github.com>
2025-01-09 10:32:33 +0100

    refactor: import with .ts extension

--- src/allImportRules.ts
+++ src/allImportRules.ts
@@ -1,6 +1,6 @@
 // https://github.com/andreashuber69/eslint-config/blob/master/README.md#----andreashuber69eslint-config
 
 import { rules } from "eslint-plugin-import";
-import { getSeverities } from "./getSeverities.js";
+import { getSeverities } from "./getSeverities.ts";
 
 export const allImportRules = getSeverities(rules, "import");
--- src/allPromiseRules.ts
+++ src/allPromiseRules.ts
@@ -1,6 +1,6 @@
 // https://github.com/andreashuber69/eslint-config/blob/master/README.md#----andreashuber69eslint-config
 
 import promise from "eslint-plugin-promise";
-import { getSeverities } from "./getSeverities.js";
+import { getSeverities } from "./getSeverities.ts";
 
 export const allPromiseRules = getSeverities(promise.rules, "promise");
